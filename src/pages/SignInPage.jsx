f95b65ca610bbfdf75e289a8d6e059a27621a62c
jefti <jeftimeira@gmail.com>
2023-07-05 22:55:41 -0300

    Requisito 1 criação do projeto

---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+import { Link, useNavigate } from "react-router-dom"
+import MyWalletLogo from "../components/MyWalletLogo"
+
+export default function SignInPage() {
+  const navigate = useNavigate();
+  function handleLogin(e){
+    e.preventDefault();
+    navigate("/home"); 
+  }
+
+  return (
+    <SingInContainer>
+      <form onSubmit={handleLogin}>
+        <MyWalletLogo />
+        <input placeholder="E-mail" type="email" />
+        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <button>Entrar</button>
+      </form>
+
+      <Link to="/cadastro">
+        Primeira vez? Cadastre-se!
+      </Link>
+    </SingInContainer>
+  )
+}
+
+const SingInContainer = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,38 @@
+import { Link, useNavigate } from "react-router-dom"
+import styled from "styled-components"
+import MyWalletLogo from "../components/MyWalletLogo"
+
+export default function SignUpPage() {
+  const navigate = useNavigate();
+  
+  function handleSignUp(e){
+    e.preventDefault();
+    navigate("/");
+  }
+
+
+  return (
+    <SingUpContainer>
+      <form onSubmit={handleSignUp}>
+        <MyWalletLogo />
+        <input placeholder="Nome" type="text" />
+        <input placeholder="E-mail" type="email" />
+        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <button>Cadastrar</button>
+      </form>
+
+      <Link to="/">
+        Já tem uma conta? Entre agora!
+      </Link>
+    </SingUpContainer>
+  )
+}
+
+const SingUpContainer = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
